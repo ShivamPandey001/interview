8f51daa29e8e4619b876424b66aaa1396fd021e4
Shivam Pandey <54200130+cse-Shivam@users.noreply.github.com>
2020-04-19 10:10:28 +0530

    Update LongestCommonSubsequence.java

--- src/com/interview/dynamic/LongestCommonSubsequence.java
+++ src/com/interview/dynamic/LongestCommonSubsequence.java
@@ -21,7 +21,7 @@ public class LongestCommonSubsequence {
     public int lcsDynamic(char str1[],char str2[]){
     
         int temp[][] = new int[str1.length + 1][str2.length + 1];
-        int max = 0;
+        int m = 0;
         for(int i=1; i < temp.length; i++){
             for(int j=1; j < temp[i].length; j++){
                 if(str1[i-1] == str2[j-1]) {
@@ -31,12 +31,12 @@ public class LongestCommonSubsequence {
                 {
                     temp[i][j] = Math.max(temp[i][j-1],temp[i-1][j]);
                 }
-                if(temp[i][j] > max){
-                    max = temp[i][j];
+                if(temp[i][j] > m){
+                    m = temp[i][j];
                 }
             }
         }
-        return max;
+        return m;
     
     }
     
